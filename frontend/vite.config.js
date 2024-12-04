bc32fe548548c45fec42dba433729d7e71a0a4a0
Rajesh <talaganarajesh@gmail.com>
2024-12-04 19:30:50 +0530

    cleaned vite config comments

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-  
-// // Add this line
-// server: {
-//   historyApiFallback: true
-// }
-
-
-// })
-
-
 import path from "path";
 
 

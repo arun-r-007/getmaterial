1280024d25235b7e215ae4248cdf1d9f899912e3
Rajesh <talaganarajesh@gmail.com>
2025-01-23 20:16:12 +0530

    skeleton loader

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -22,6 +22,9 @@ import { getDocs, collection } from "firebase/firestore";
 
 import { Heart } from "lucide-react";
 
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 
 const TopContributor = ({ topContributor }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -342,7 +345,7 @@ function Dashboard() {
 
 
 
-          
+
 
           {/* module Filter */}
           <div>
@@ -383,183 +386,223 @@ function Dashboard() {
         </div>
       </div>
 
-      {loading && <div className='flex justify-center mt-32'><span className="loader"></span></div>}
       {error && <p className="text-red-500">Error fetching notes: {error}</p>}
 
+
+
       {/* Notes Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      {loading ?
+
+        <div className="flex-row justify-center gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {[1, 2, 3].map((card) => (
+            <div
+              key={card}
+              className="z-70 w-full bg-white rounded-lg shadow-lg p-4 flex flex-row space-x-6 overflow-hidden"
+            >
+              {/* Left Section Skeletons */}
+              <div className="flex flex-col justify-center flex-grow">
+                <Skeleton height={30} width={200} className="mb-4" /> {/* Title */}
+                <Skeleton height={20} width={140} className="mb-4" /> {/* Subtitle */}
+                <Skeleton height={20} width={180} className="mb-4" /> {/* Subtitle */}
+
+                <div className='flex flex-row gap-2'>
+                  <Skeleton height={40} width={100} className="mt-4" /> {/* Button */}
+                  <Skeleton height={40} width={40} className="mt-4" /> {/* likes */}
+                </div>
+              </div>
+
+              {/* Right Section Skeleton */}
+              <div className="flex-shrink-0">
+                <Skeleton height={160} width={150} className="rounded-lg" /> {/* Image Placeholder */}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        :
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
 
-        {filteredNotes.map((note) => (
-          <div key={note.id} className="bg-white p-5 rounded-xl shadow-xl flex flex-row justify-between">
+          {filteredNotes.map((note) => (
+            <div key={note.id} className="bg-white p-5 rounded-xl shadow-xl flex flex-row justify-between">
 
-            <div className='flex flex-col justify-between'>
+              <div className='flex flex-col justify-between'>
 
-              <h1 className='text-xl font-bold mb-2'>
-                <span
-                  onClick={() => {
-                    if (subjectFilter == note.subject) {
-                      setSubjectFilter("")
+                <h1 className='text-xl font-bold mb-2'>
+                  <span
+                    onClick={() => {
+                      if (subjectFilter == note.subject) {
+                        setSubjectFilter("")
+                      }
+                      else {
+                        setSubjectFilter(note.subject)
+                      }
                     }
-                    else {
-                      setSubjectFilter(note.subject)
                     }
-                  }
-                  }
-                  className='group hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
-                >
-                  {note.subject || "unknown"}
+                    className='group hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
+                  >
+                    {note.subject || "unknown"}
 
-                  {/* Tooltip */}
-                  <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    View all notes of {note.subject}
+                    {/* Tooltip */}
+                    <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                      View all notes of {note.subject}
+                    </span>
                   </span>
-                </span>
-              </h1>
+                </h1>
 
-              <p className='text-gray-600 mb-2'>@
-                <span
-                  onClick={() => {
-                    if (moduleFilter == note.module) {
-                      setModuleFilter("")
+                <p className='text-gray-600 mb-2'>@
+                  <span
+                    onClick={() => {
+                      if (moduleFilter == note.module) {
+                        setModuleFilter("")
+                      }
+                      else {
+                        setModuleFilter(note.module)
+                      }
                     }
-                    else {
-                      setModuleFilter(note.module)
                     }
-                  }
-                  }
-                  className='text-gray-600 group hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
-                >
-                  {note.module || "unknown"}
+                    className='text-gray-600 group hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
+                  >
+                    {note.module || "unknown"}
 
-                  {/* Tooltip */}
-                  <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    View all notes of {note.module}
+                    {/* Tooltip */}
+                    <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                      View all notes of {note.module}
+                    </span>
                   </span>
-                </span>
-              </p>
+                </p>
 
 
-              <p className='text-gray-600 mb-2'>Semester:
-                <span
-                  onClick={() => {
-                    if (semesterFilter == note.semester) {
-                      setSemesterFilter("")
+                <p className='text-gray-600 mb-2'>Semester:
+                  <span
+                    onClick={() => {
+                      if (semesterFilter == note.semester) {
+                        setSemesterFilter("")
+                      }
+                      else {
+                        setSemesterFilter(note.semester)
+                      }
                     }
-                    else {
-                      setSemesterFilter(note.semester)
+
                     }
-                  }
+                    className='text-gray-600 text-center group ml-1 hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
+                  >
+                    {note.semester || "unknown"}
 
-                  }
-                  className='text-gray-600 text-center group ml-1 hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
-                >
-                  {note.semester || "unknown"}
-
-                  {/* Tooltip */}
-                  <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    View all notes of sem {note.semester}
+                    {/* Tooltip */}
+                    <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                      View all notes of sem {note.semester}
+                    </span>
                   </span>
-                </span>
-              </p>
+                </p>
 
 
-              <h2 className="mb-2 text-gray-600">Details: {note.name}</h2>
+                <h2 className="mb-2 text-gray-600">Details: {note.name}</h2>
 
 
-              <p className='text-gray-600 mb-4'>Uploaded by:
-                <span
-                  onClick={() => {
-                    if (nameFilter == note.contributorName) {
-                      setNameFilter("")
+                <p className='text-gray-600 mb-4'>Uploaded by:
+                  <span
+                    onClick={() => {
+                      if (nameFilter == note.contributorName) {
+                        setNameFilter("")
+                      }
+                      else {
+                        setNameFilter(note.contributorName)
+                      }
                     }
-                    else {
-                      setNameFilter(note.contributorName)
                     }
-                  }
-                  }
-                  className='text-green-800 group font-semibold hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
-                >
-                  {note.contributorName || "unknown"}
+                    className='text-green-800 group font-semibold hover:text-green-500 transition-colors duration-300 cursor-pointer relative'
+                  >
+                    {note.contributorName || "unknown"}
 
-                  {/* Tooltip */}
-                  <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    View all notes by {note.contributorName}
+                    {/* Tooltip */}
+                    <span className="tooltip absolute bottom-full w-full transform -translate-x-1/2 mt-2 py-3 px-2 bg-slate-900 text-white text-xs rounded-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                      View all notes by {note.contributorName}
+                    </span>
                   </span>
-                </span>
-              </p>
+                </p>
 
 
-              <div className='flex flex-row justify-start gap-1 items-center'>
+                <div className='flex flex-row justify-start gap-1 items-center'>
 
-                <a
-                  href={note.fileUrl}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="text-white bg-black py-2 text-center text-xs md:text-sm md:w-fit md:px-3 w-20 rounded-lg hover:rounded-2xl transition-all duration-300"
-                >
-                  View Note
-                </a>
+                  <a
+                    href={note.fileUrl}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-white bg-black py-2 text-center text-xs md:text-sm md:w-fit md:px-3 w-20 rounded-lg hover:rounded-2xl transition-all duration-300"
+                  >
+                    View Note
+                  </a>
 
-                <div className='flex flex-row bg-gray-100 md:px-2 p-1 rounded-lg md:hover:bg-gray-200 transition-all'>
-                  <Heart style={{
-                    cursor: "pointer",
-                    marginRight: "0px",
-                    color: "gray", // Toggle color based on the `liked` state
-                  }} onClick={() => handleLike(note.id)} className={isLiked[note.id] ? " fill-red-500 rounded-md transition-all" :"bg-transparent md:hover:fill-red-400 md:hover:p-0.5 rounded-full transition-all"} />
+                  <div className='flex flex-row bg-gray-100 md:px-2 p-1 rounded-lg md:hover:bg-gray-200 transition-all'>
+                    <Heart style={{
+                      cursor: "pointer",
+                      marginRight: "0px",
+                      color: "gray", // Toggle color based on the `liked` state
+                    }} onClick={() => handleLike(note.id)} className={isLiked[note.id] ? " fill-red-500 rounded-md transition-all" : "bg-transparent md:hover:fill-red-400 md:hover:p-0.5 rounded-full transition-all"} />
 
-                  {allLikes[note.id] || 0}
+                    {allLikes[note.id] || 0}
+
+                  </div>
+
+                  {admin && ( // Show Delete button only for admin
+                    <div className="bg-slate-200 rounded-lg p-2 hover:rounded-xl transition-all duration-300">
+                      <button onClick={() => handleDelete(note.id)}>
+                        <Trash size={20} color="red" />
+                      </button>
+                    </div>
+                  )}
+
+                  {owner && owner.email == note.metadata.createdBy && (
+                    <div className="bg-slate-100 rounded-lg md:px-2 p-1 hover:bg-slate-200 hover:rounded-xl transition-all duration-300">
+                      <button onClick={() => handleDelete(note.id)}>
+                        <Trash size={20} color="red" />
+                      </button>
+                    </div>
+                  )}
 
                 </div>
 
-                {admin && ( // Show Delete button only for admin
-                  <div className="bg-slate-200 rounded-lg p-2 hover:rounded-xl transition-all duration-300">
-                    <button onClick={() => handleDelete(note.id)}>
-                    <Trash size={20} color="red" />
-                    </button>
-                  </div>
-                )}
 
-                {owner && owner.email == note.metadata.createdBy && (
-                  <div className="bg-slate-100 rounded-lg md:px-2 p-1 hover:bg-slate-200 hover:rounded-xl transition-all duration-300">
-                    <button onClick={() => handleDelete(note.id)}> 
-                      <Trash size={20} color="red" />
-                    </button>
-                  </div>
-                )}
+              </div>
+
+              <div className='flex flex-col items-center justify-between'>
+                <img
+                  src={getPDFPreviewUrl(extractFileIdFromUrl(note.fileUrl))}
+                  alt="PDF Preview"
+                  className="md:w-40 md:h-48 w-28 h-36  object-cover rounded-lg  ml-2 border-2 border-gray-300"
+                />
+                <p className='opacity-40 bottom-0'>{note.uploadedAt.toDate().toLocaleDateString('en-GB')}</p>
+
 
               </div>
 
 
-            </div>
-
-            <div className='flex flex-col items-center justify-between'>
-              <img
-                src={getPDFPreviewUrl(extractFileIdFromUrl(note.fileUrl))}
-                alt="PDF Preview"
-                className="md:w-40 md:h-48 w-28 h-36  object-cover rounded-lg  ml-2 border-2 border-gray-300"
-              />
-              <p className='opacity-40 bottom-0'>{note.uploadedAt.toDate().toLocaleDateString('en-GB')}</p>
-
 
             </div>
 
 
 
-          </div>
+
+          ))}
+
+          {/* No results message */}
+          {filteredNotes.length === 0 && !loading && (
+            <div className="col-span-full text-center text-gray-500 py-8">
+              No notes found. Try adjusting your filters.
+            </div>
+          )}
+        </div>
+
+
+      }
 
 
 
 
-        ))}
 
-        {/* No results message */}
-        {filteredNotes.length === 0 && !loading && (
-          <div className="col-span-full text-center text-gray-500 py-8">
-            No notes found. Try adjusting your filters.
-          </div>
-        )}
-      </div>
       <div className='text-center opacity-90 pt-14 flex flex-col'>
         <a href="https://talaganarajesh.vercel.app/" target='_blank' rel="noopener noreferrer" className='hover:-rotate-3 transition-all duration-300'>
           <span className='text-green-700 font-bold'><span className='text-black'>~ by</span> Rajesh</span>
